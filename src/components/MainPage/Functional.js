591fdc8a7fe1e0baba840d20a73aa76993712ba7
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2018-12-11 13:49:28 +0200

    Added test routing for menu + 3 files

---
+++ src/components/MainPage/Functional.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Functional = () => (
+    <div>
+        <h1>Hello Functional Component</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur deleniti dolorem, ea eligendi ipsam maiores, minima modi non odit officia, quidem saepe sit voluptatum! Eius magnam officia suscipit voluptas voluptatibus.</p>
+    </div>
+);
+
+export default Functional;
---
+++ src/components/MainPage/Tests.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Tests = () => (
+    <div>
+        <h1>Welcome to Test Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi atque consectetur cumque doloribus ea earum est, eveniet iste magnam nesciunt nobis non obcaecati officiis praesentium quidem quo rerum temporibus vel.</p>
+    </div>
+);
+
+export default Tests;
